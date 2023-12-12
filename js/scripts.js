b278b0040e16b0d280553f311b3a0a354b8f2d7f
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2023-12-12 16:51:41 +0100

    FizzBuzz

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,21 @@ Numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
     //console.log(i);
 
-    if (i % 3 == 0) {
+    if ((i % 3 == 0) & (i % 5 == 0)) {
+        console.log('FizzBuzz', i);
+    }
+
+    else if (i % 3 == 0) {
         console.log('Fizz', i);
 
     }
+
     else if (i % 5 == 0) {
         console.log('Buzz', i);
     }
+ 
     else {
         console.log(i);
     }
-
     
 }
