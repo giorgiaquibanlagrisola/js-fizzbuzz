8a6a2a80d1c6dcd4cfe22554a082c39c3a1eea56
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2023-12-12 16:45:52 +0100

    Multipli di 3 e multipli di 5

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/*
+Numeri da 1 a 100
+-Se il numero è multiplo di 3 stampo in console Fizz
+-Se il numero è multiplo di 5 spampo in console Buzz
+-Se il numero è multiplo sia di 3 che di 5 stampo in console FizzBuzz
+*/
+
+for (let i = 1; i <= 100; i++) {
+    //console.log(i);
+
+    if (i % 3 == 0) {
+        console.log('Fizz', i);
+
+    }
+    else if (i % 5 == 0) {
+        console.log('Buzz', i);
+    }
+    else {
+        console.log(i);
+    }
+
+    
+}
